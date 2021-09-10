5c8c910d47a5088907011e2ae1de68cc195050db
Luke Boelling <lboel@users.noreply.github.com>
2021-09-10 09:32:46 +0200

    Updating logic for token generation. To avoid to many token generations. v0.1.2

--- R/getAccessTokenByUsernameAndPassword.R
+++ R/getAccessTokenByUsernameAndPassword.R
@@ -1,7 +1,7 @@
 #' Creates an API-Access-Token on Gitlab.Pavlovia.Org by using username/password.
 #'
-#' Tokens named PavloviaR_#Random_String#. Tokens should be manually saved for a later use.
-#' Do not use this function every time you use the other functions.Run it once and save the access token at secure location.
+#' Tokens named PavloviaR_#Random_String#. Tokens are saved to accessToken.csv in the current working directory and will be reused.
+#' So we avoid creating an unlimited number of accessTokens on gitlab.pavlovia.org. Be careful with the token!
 #'
 #' @param username Username or Email of a registered account in Pavlovia
 #' @param password Passord for the Pavlovia/Gitlab.Pavlovia Account
@@ -11,8 +11,10 @@
 #' @examples tokenResponseObject <- getAccessTokenByUsernameAndPassword("username", "12345")
 #' tokenResponseObject$data$token #here is your accessToken
 getAccessTokenByUsernameAndPassword <- function(username,password)
-{
-  responseObject <- list(data = NULL, message = "OK", isError = F)
+{  responseObject <- list(data = NULL, message = "OK", isError = F)
+
+  if(!file.exists("accessToken.csv"))
+  {
   nameOfAccessToken <- paste0("PavloviaR_",stringi::stri_rand_strings(1, 6, pattern = "[A-Za-z0-9]"))
   session <-rvest::session("https://gitlab.pavlovia.org/profile/personal_access_tokens")
 
@@ -34,12 +36,22 @@ getAccessTokenByUsernameAndPassword <- function(username,password)
       rvest::html_node('#created-personal-access-token') %>%
       rvest::html_attr('value')
     responseData <- list(name=nameOfAccessToken,token=accessToken)
+    readr::write.csv2( dplyr::tibble(name=nameOfAccessToken,token=accessToken),file = "accessToken.csv")
     responseObject$data <-  responseData
   }
   else {
     responseObject$isError <- T
     responseObject$message <- "Token could not be generated"
   }
+  }
+  else
+  {
+    print("Using available token from working directory")
+    tokenData <- invisible(readr::read_delim("accessToken.csv",delim = ";", show_col_types = F))
+    responseData <- list(name=tokenData$name,token=tokenData$token)
+    responseObject$data <-  responseData
 
+
+    }
   return(responseObject)
 }
